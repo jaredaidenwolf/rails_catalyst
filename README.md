ef144f136469323022979be13c9940457e5bffb2
Jared Aiden Wolf <jared@jaredaidenwolf.com>
2024-04-22 09:06:13 -0400

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # RailsCatalyst
 
-RailsCatalyst accelerates Rails app creation by providing a pre-configured template.
+This Rails application template automates the setup process for new Rails projects. It includes a series of customizations and configurations that are commonly required for modern web applications. This includes:
+
+- Setting up essential gems
+- Configuring PostgreSQL for database management
+- Initializing version control settings
+- Creating environment variables
+- Establishing a basic homepage with header partials
+
+The goal is to accelerate the development process by pre-configuring repetitive setup tasks, allowing developers to focus on the unique aspects of their applications.
--- rails_catalyst.rb
+++ rails_catalyst.rb
@@ -1,13 +1,3 @@
-# RailsCatalyst Template
-#
-# This Rails application template automates the setup process for new Rails projects.
-# It includes a series of customizations and configurations that are commonly required
-# for modern web applications. This includes setting up essential gems, configuring
-# PostgreSQL for database management, initializing version control settings, creating
-# environment variables, and establishing a basic homepage with header partials.
-# The goal is to accelerate the development process by pre-configuring repetitive
-# setup tasks, allowing developers to focus on the unique aspects of their applications.
-
 require_relative "templates/append_gemfile"
 require_relative "templates/append_gitignore"
 require_relative "templates/create_env_file"
